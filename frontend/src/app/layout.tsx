f08d8cc0dcc7ba5571ccf99338b6456257968188
Rachel Park <rachelsupark@gmail.com>
2023-05-31 15:03:27 -0700

    social image

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,6 +18,22 @@ export default function RootLayout({
     <>
     <Head>
       <link rel="icon" href="/a-favicon.ico"/>
+      <title>Anything</title>
+      <meta
+        name="description"
+        content="Click and fill anything with a simple text prompt."
+      />
+
+      <meta property="og:title" content="Anything AI: a generative photo editing tool" />
+      <meta
+        property="og:description"
+        content="Click and fill anything with a simple text prompt."
+      />
+      <meta
+        property="og:image"
+        content="https://anything-ai.com/social-image.png"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
     <html lang="en">
       <body className={inter.className}>{children}</body>
